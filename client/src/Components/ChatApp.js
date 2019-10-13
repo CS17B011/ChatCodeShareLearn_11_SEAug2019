cdb6e841c5e2412232f567c1f70b296ea9ebcf0c
CS17B011 <cs17b011@iittp.ac.in>
2019-10-13 22:54:23 +0530

    Added Chat Room Functionality

--- client/src/Components/ChatApp.js
+++ client/src/Components/ChatApp.js
@@ -3,8 +3,44 @@ import SidePanel from './SidePanel';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import './ChatApp.css';
+import io from 'socket.io-client';
 
 class ChatApp extends Component {
+
+	socket = io('http://localhost:4000');
+
+	constructor(props){
+		super(props);
+		this.state = {
+			msgs: [],
+			contacts: []
+		};
+	}
+
+	addMsg = (val,cls) => {
+		console.log(this.socket);
+		var data = {
+			cls,
+			val,
+			id: this.socket.id
+		};
+		this.setState({msgs: [...this.state.msgs,data]});
+		this.socket.emit('newmessage',data);
+	};
+
+	componentDidMount() {
+		this.socket.on('sendmessages',data => {
+			console.log("New Message...");
+			if(this.socket.id!==data.id){
+				var newMessage = {
+					cls:"sent",
+					val:data.val
+				};
+				this.setState({msgs:[...this.state.msgs,newMessage]});
+			}
+		});
+	}
+	
 
     render() {
         return (
@@ -20,8 +56,8 @@ class ChatApp extends Component {
 			                <i className="fa fa-google" aria-hidden="true" />
 		              	</div>
 		            </div>
-                    <Messages/>
-                    <MessageInput/>
+                    <Messages msgs={this.state.msgs}/>
+                    <MessageInput submit={this.addMsg}/>
     	        </div>
 	        </div>
         );
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const socketio = require('socket.io');
 const http = require('http');
 const server = http.createServer(app);
 const io = socketio(server);
+const path = require('path');
 
 const bodyParser = require('body-parser');
 const PORT =  process.env.PORT || 4000;
@@ -13,24 +14,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-/*
 // Set static folder
 app.use(express.static('./client/build'));
 app.get('*', (req, res) => {
 res.sendFile(path.resolve( __dirname ,'client', 'build', 'index.html'));
 });
-*/
 
 app.get('/', (req,res) => {
   res.send("<h1>Chat_Code_Share_Learn Backend</h1>");
 });
 
+connections = []
+users = []
 
 //Online Chat Service
-io.on('connection', (client) => {
-  console.log("Connected...");
-  client.on('disconnect',() => {
-    console.log("Disconnected...");
+io.on('connection', (socket) => {
+  connections.push(socket);
+  console.log('Connected... : %s connected.',connections.length);
+  socket.on('disconnect',() => {
+		connections.splice(connections.indexOf(socket.id),1);
+    console.log('Disconnected... : %s remaining.', connections.length);
+  });
+  socket.on('newmessage',data => {
+    console.log(data.val);
+    io.sockets.emit('sendmessages',data);
   });
 });
 
