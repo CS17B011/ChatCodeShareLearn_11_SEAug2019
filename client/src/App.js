07a72a8fef9f486b1b0cd5281e4e4320a9d20e5a
Liveitabhi <cs17b002@iittp.ac.in>
2019-10-14 11:47:06 +0530

    Added Editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import ChatApp from './Components/ChatApp';
+import Editor from './Components/Editor';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <Router>
+            <Route exact path="/editor">
+                <Editor/>
+            </Route>
+            <Route exact path="/">
                 <ChatApp/>
-            </div>
+            </Route>
+            </Router>
         );
     }
 }
---
+++ client/src/Components/Editor.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import Pusher from 'pusher-js';
+import pushid from 'pushid';
+import axios from 'axios'
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+
+export default class Editor extends Component {
+    constructor() {
+        super();
+        this.state = {
+          id: '',
+          html: '',
+          css: '',
+          js: '',
+        };
+      }
+
+      componentDidUpdate() {
+        this.runCode();
+      }
+
+      componentDidMount() {
+        this.setState({
+          id: pushid(),
+        });
+      }
+
+      runCode = () => {
+        /*const { html, js } = this.state;
+
+        const iframe = this.refs.iframe;
+        const document = iframe.contentDocument;
+        const documentContents = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+          </head>
+          <body>
+            ${html}
+            <script type="text/javascript">
+              ${js}
+            </script>
+          </body>
+          </html>
+        `;
+        document.open();
+        document.write(documentContents);
+        document.close();*/
+      };
+
+      render() {
+        const { html, js } = this.state;
+        const codeMirrorOptions = {
+          theme: 'material',
+          lineNumbers: true,
+          scrollbarStyle: null,
+          lineWrapping: true,
+        };
+
+        return (
+          <div className="App">
+            <section className="playground">
+              <div className="code-editor html-code">
+                <div className="editor-header">HTML</div>
+                <CodeMirror
+                  value={html}
+                  options={{
+                    mode: 'htmlmixed',
+                    ...codeMirrorOptions,
+                  }}
+                  onBeforeChange={(editor, data, html) => {
+                    this.setState({ html });
+                  }}
+                />
+              </div>
+              
+              <div className="code-editor js-code">
+                <div className="editor-header">JavaScript</div>
+                <CodeMirror
+                  value={js}
+                  options={{
+                    mode: 'javascript',
+                    ...codeMirrorOptions,
+                  }}
+                  onBeforeChange={(editor, data, js) => {
+                    this.setState({ js });
+                  }}
+                />
+              </div>
+            </section>
+            {/*
+            <section className="result">
+              <iframe title="result" className="iframe" ref="iframe" />
+            </section>
+        */}
+          </div>
+        );
+      }
+    }
