9ab304bb6ed56e55001818e7af2bb3a05c783935
Liveitabhi <cs17b002@iittp.ac.in>
2019-11-20 10:43:34 +0530

    Login component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ChatApp from './Components/ChatApp/ChatApp';
 import Editor from './Components/Editor/Editor';
+import Login from './Components/Login/js/Login'
 import {
 	BrowserRouter as Router,
 	Route,
@@ -11,14 +12,15 @@ class App extends Component {
     render() {
         return (
             <Router>
-            
             <Route exact path="/">
                 <ChatApp/>
             </Route>
             <Route exact path="/editor">
                 <Editor/>
             </Route>
-            
+            <Route exact path="/login">
+                <Login/>
+            </Route>
             </Router>
         );
     }
