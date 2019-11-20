7e0c264477c9339c7d01c725f01b53e0760b671c
arnabcs17b006 <cs17b006@iittp.ac.in>
2019-11-20 17:54:43 +0530

    support for 40 programming languages in editor added

--- client/src/Components/Editor/Editor.js
+++ client/src/Components/Editor/Editor.js
@@ -9,11 +9,46 @@ import 'codemirror/theme/material.css';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/apl/apl';
+import 'codemirror/mode/asterisk/asterisk';
+import 'codemirror/mode/brainfuck/brainfuck';
+import 'codemirror/mode/clojure/clojure';
+import 'codemirror/mode/cobol/cobol';
+import 'codemirror/mode/coffeescript/coffeescript';
+import 'codemirror/mode/commonlisp/commonlisp';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/d/d';
+import 'codemirror/mode/dart/dart';
+import 'codemirror/mode/fortran/fortran';
+import 'codemirror/mode/haskell/haskell';
+import 'codemirror/mode/groovy/groovy';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/swift/swift';
+import 'codemirror/mode/vhdl/vhdl';
+
+
+
 
 var defaults = {
   markdown: '',
   javascript: '',
-  htmlmixed: ''
+  htmlmixed: '',
+  apl: '',
+  asterisk: '',
+  brainfuck: '',
+  clojure: '',
+  cobol: '',
+  coffeescript: '',
+  commonlisp: '',
+  css: '',
+  d: '',
+  dart: '',
+  fortran: '',
+  haskell: '',
+  groovy: '',
+  xml: '',
+  swift: '',
+  vhdl: ''
 };
 
 export default class Editor extends Component {
@@ -64,6 +99,21 @@ export default class Editor extends Component {
             <option value="markdown">Markdown</option>
             <option value="javascript">JavaScript</option>
             <option value="htmlmixed">htmlmixed</option>
+            <option value="apl">apl</option>
+            <option value="asterisk">asterisk</option>
+            <option value="brainfuck">brainfuck</option>
+            <option value="clojure">clojure</option>
+            <option value="coffeescript">coffeescript</option>
+            <option value="commonlisp">commonlisp</option>
+            <option value="css">css</option>
+            <option value="d">d</option>
+            <option value="dart">dart</option>
+            <option value="fortran">fortran</option>
+            <option value="haskell">haskell</option>
+            <option value="groovy">groovy</option>
+            <option value="xml">xml</option>
+            <option value="swift">swift</option>
+            <option value="vhdl">vhdl</option>
           </select>
           <button onClick={this.toggleReadOnly}>Toggle read-only mode (currently {this.state.readOnly ? 'on' : 'off'})</button>
         </div>
